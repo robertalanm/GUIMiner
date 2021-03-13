29eb15c2dcb128f9224ece9a3d7cb7ca5b311805
alex <amaxwell@>
2021-03-13 17:00:46 -0600

    added working functionality for different datasets, networks, and stop mining button

--- external_programs/python_example.py
+++ external_programs/python_example.py
@@ -2,14 +2,17 @@ import sys
 import os
 import argparse
 # FUNCTIONS
+parser = argparse.ArgumentParser()
+parser.add_argument('-d','--dataset',metavar="dataset",type=str,help="Dataset options, gpt2_wiki, gpt2_genesis, etc",default="gpt2_wiki/gpt2_wiki.py")
+parser.add_argument('-n','--network',metavar="network",type=str,help="Subtensor network between Kusanagi and Akira, Kusanagi recommended",default="kusanagi")
+parser.add_argument('-c','--command',metavar="command",type=bool,help="Start or stop options for Mining",default=True)
 
-def main(dataset = "gpt2_wiki/gpt2_wiki.py", network = "kusanagi"):
-    print("running os.system command")
+args=parser.parse_args()
+print(args)
+while args.command:
     try:
-        os.system(f"python ~/.bittensor/bittensor/miners/TEXT/{dataset} --subtensor.network {network}|tee -a log.txt")
+        os.system(f"python ~/.bittensor/bittensor/miners/TEXT/{args.dataset} --subtensor.network {args.network}|tee -a log.txt")
     except Exception as e:
         print(e)
-
-
-if __name__ == "__main__":
-    main()
+else:
+    exit(0)
